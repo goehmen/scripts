bf3cf288e0d1907c5194c65bd3ddd8d357873186
Marco Nicosia <mnicosia@pivotal.io>
2017-09-11 11:54:54 -0700

    - auto-discover the latest version if no version specified - add a debug flag

--- core-services-accept/dl-cf-mysql-rc.sh
+++ core-services-accept/dl-cf-mysql-rc.sh
@@ -4,6 +4,7 @@ set -e
 
 # API_TOKEN=${API_TOKEN:?[ERROR]: API_TOKEN environment variable must be set.}
 PRIVATE=0
+VERSION=""
 
 usage="\
 $0 -r version
@@ -14,9 +15,10 @@ Options:
     -h                This help
     -p                Specify that this is PCF, not an OSS RC
     -r version        Specify the version to be downloaded, e.g. 24.21.0
+    -d                Debug mode
 "
 
-args=`getopt phr: $*`; errcode=$?; set -- $args
+args=`getopt dphr: $*`; errcode=$?; set -- $args
 if [ 0 -ne $errcode ]; then echo ; echo "$usage" ; exit $errcode ; fi
 
 for i ; do
@@ -32,18 +34,28 @@ for i ; do
         -r)
             VERSION=$2
             shift ; shift ;;
+        -d)
+            set -x
+            shift ; 
     esac
 done
 
 if [ "--" == $1 ]; then shift; fi
 
 if [ 1 == $PRIVATE ]; then
-    s3File="s3://pcf-mysql-releases/final/cf-mysql-${VERSION}.tgz"
+    s3Path="s3://pcf-mysql-releases/final/"
 else
-    s3File="s3://cf-mysql-releases/final/cf-mysql-${VERSION}.tgz"
+    s3Path="s3://cf-mysql-releases/final/"
 fi    
 
-aws s3 cp ${s3File} .
+if [ "X" == $VERSION"X" ]; then
+    rcFile=$(aws s3 ls ${s3Path} | gsort -V -rk4 | awk 'NR == 1 {print $4}')
+else
+    rcFile="cf-mysql-${VERSION}.tgz"
+fi
+
+
+aws s3 cp "${s3Path}${rcFile}" .
 errcode=$?
 
 if [ 0 -ne $errcode ]; then
