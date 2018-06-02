c2f96c01c67662c42047f3f754f5ac2e3c3b0406
Marco Nicosia <mnicosia@pivotal.io>
2018-06-01 17:35:57 -0700

    Edits to work for v2 tiles as well.

--- core-services-accept/tile-components.sh
+++ core-services-accept/tile-components.sh
@@ -19,20 +19,24 @@ Options:
     -r version        Specify the version to be downloaded, e.g. 1.9.4
     -t tmpdir         Specify a working directory to use (optional. default /var/tmp/t)
     -i profile        AWS profile to specify when running s3 client (optional.)
+    -v                debug mode
     component         Which version to validate, e.g. cf-mysql, stemcell, ...
 "
 
-args=`getopt hxp:r:t:i: $*`; errcode=$?; set -- $args
+args=`getopt vhxp:r:t:i: $*`; errcode=$?; set -- $args
 if [ 0 -ne $errcode ]; then echo ; echo "$usage" ; exit $errcode ; fi
 
 for i ; do
     case $i in
+        -v)
+	    set -x ; shift ;
+            ;;
         -h)
             echo "$usage" ; exit 0
             ;;
         -p)
             PRODUCT=$2
-            product_file=$(echo $PRODUCT | tr '-' '_').yml
+            product_file="*.yml"
             shift ; shift ;;
         -r)
             VERSION=$2
@@ -49,7 +53,7 @@ for i ; do
     esac
 done
 
-if [ "XX" = "${PRODDUCT}" -o "XX" = "${VERSION}" ]; then
+if [ "XX" = "${PRODUCT}" -o "XX" = "${VERSION}" ]; then
     echo "[ERROR] Please specify product and version." ; echo
     echo "$usage"
     exit -1
@@ -68,6 +72,18 @@ fi
 mkdir -p ${WORKDIR}/${SUBDIR} ; cd ${WORKDIR}/${SUBDIR} ;
 
 case ${VERSION} in
+    2.3*)
+	s3Path="s3://dedicated-mysql-tile-blobs/p.mysql-2.3/tested-tiles/"
+	;;
+    2.2*)
+	s3Path="s3://dedicated-mysql-tile-blobs/p.mysql-2.2/tested-tiles/"
+	;;
+    2.1*)
+	s3Path="s3://dedicated-mysql-tile-blobs/p.mysql-2.1/tested-tiles/"
+	;;
+    2.0*)
+	s3Path="s3://dedicated-mysql-tile-blobs/p.mysql-2.0/tested-tiles/"
+        ;;
     1.10*)
         s3Path="s3://pcf-core-services-artifacts/p-mysql/master/tested/"
         ;;
@@ -85,7 +101,7 @@ esac
 # If it hasn't already been downloaded, get the most recent RC in the correct AWS bucket
 s3File=$(aws --profile=$PROFILE s3 ls $s3Path | grep $VERSION | sort -r | awk 'NR == 1 {print $4}')
 if [ ! -f ${WORKDIR}/${s3File} ]; then 
-    aws s3 cp ${s3Path}${s3File} ${WORKDIR}
+    aws --profile=$PROFILE s3 cp ${s3Path}${s3File} ${WORKDIR}
 fi
 unzip -q ${WORKDIR}/${s3File}
 
